bc0ac481159cf77a4087dd327d1d7ec6f673c35e
Lev Himmelfarb <lev@boylesoftware.com>
2017-01-18 17:23:32 -0500

    Split logic into internal modules. Extracted common logic into the x2node-common module.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# x2node-query
-SQL query builder.
+# X2 Framework for Node.js | Query Builder
+
+This module provides a SQL query builder that together with [x2node-rsparser](https://www.npmjs.com/package/x2node-rsparser) module allows applications performing complex SQL database operations while concentrating on the high level data structures and the business logic. The module is a part of X2 Framework.
+
+**THIS IS WORK IN PROGRESS**
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Database query builder module.
+ *
+ * @module x2node-query
+ */
+'use strict';
+
+const common = require('x2node-common');
+
+const QueryFactory = require('./lib/query-factory.js');
+
+
+/**
+ * The drivers registry.
+ *
+ * @private
+ */
+const DRIVERS = {
+	'mysql': new (require('./lib/driver/mysql-driver.js'))(),
+	'pg': new (require('./lib/driver/pg-driver.js'))()
+};
+
+/**
+ * Create new factory using the specified database driver. Once created, the
+ * factory instance can be used by the application throughout its life cycle.
+ *
+ * @param {string} dbDriverName Database driver name. Out of the box, two drivers
+ * are available: "mysql" (for
+ * {@link https://www.npmjs.com/package/mysql} and compatible others) and
+ * "pg" (for {@link https://www.npmjs.com/package/pg}). Additional drivers can be
+ * registered using {@link registerDriver} function before creating the factory.
+ * @returns {module:x2node-query~QueryFactory} Query factory instance.
+ * @throws {module:x2node-common.X2UsageError} If the provided driver name is
+ * invalid.
+ */
+exports.createQueryFactory = function(dbDriverName) {
+
+	// lookup the driver
+	const dbDriver = DRIVERS[dbDriverName];
+	if (dbDriver === undefined)
+		throw new common.X2UsageError(
+			'Invalid database driver "' + dbDriverName + '".');
+
+	// create and return the factory
+	return new QueryFactory(dbDriver);
+};
+
+/**
+ * Register custom database driver. After the driver is registered, a query
+ * factory instance can be created using the driver.
+ *
+ * @param {string} dbDriverName Database driver name.
+ * @param {module:x2node-query.DBDriver} dbDriver Driver implementation.
+ */
+exports.registerDriver = function(dbDriverName, dbDriver) {
+
+	DRIVERS[dbDriverName] = dbDriver;
+};
