d377222000e03bf7b199f4887a45151ff86c7700
Lev Himmelfarb <lev@boylesoftware.com>
2017-03-30 17:55:03 -0400

    Work in progress.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
  * @module x2node-dbos
  * @requires module:x2node-common
  * @requires module:x2node-records
+ * @requires module:x2node-patch
  * @requires module:x2node-rsparser
  * @implements {module:x2node-records.Extension}
  */
