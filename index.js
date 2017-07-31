0ddc84ffbdcbd86339a4f0452dc111ddf0c885fb
Adrian Jagnanan <ajagnanan@gmail.com>
2017-07-31 11:26:29 -0400

    copy mysql driver

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ const orderBuilder = require('./lib/order-builder.js');
  */
 const DRIVERS = {
 	'mysql': require('./lib/driver/mysql-driver.js'),
-	'pg': require('./lib/driver/pg-driver.js')
+	'pg': require('./lib/driver/pg-driver.js'),
+    'sqlite': require('./lib/driver/sqlite-driver.js')
 };
 
 /**
